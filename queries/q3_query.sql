912558c0d247b39eb9e0c2f2a30f30295719362a
ironholds <ironholds@gmail.com>
2015-05-24 18:52:26 -0400

    crap crap crap crap crap

--- queries/q3_query.sql
+++ queries/q3_query.sql
@@ -2,6 +2,5 @@ SELECT year, month, day, COUNT(*) AS pageviews
 FROM wmf.webrequest
 WHERE year = 2015
 AND month = 04
-AND webrequest_source IN('text','mobile')
 AND geocoded_data['country_code'] != 'US'
 GROUP BY year, month, day;
